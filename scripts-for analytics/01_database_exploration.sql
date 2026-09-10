9d29ec8c91ef9bae3458cfbdde7ef65fded5936a
kolisah10 <166951597+kolisah10@users.noreply.github.com>
2026-09-11 00:09:30 +0530

    Update 01_database_exploration.sql

--- scripts-for analytics/01_database_exploration.sql
+++ scripts-for analytics/01_database_exploration.sql
@@ -1,114 +1,30 @@
 /*
-=============================================================
-Create Database and Schemas
-=============================================================
-Script Purpose:
-    This script creates a new database named 'DataWarehouseAnalytics' after checking if it already exists. 
-    If the database exists, it is dropped and recreated. Additionally, this script creates a schema called gold
-	
-WARNING:
-    Running this script will drop the entire 'DataWarehouseAnalytics' database if it exists. 
-    All data in the database will be permanently deleted. Proceed with caution 
-    and ensure you have proper backups before running this script.
+===============================================================================
+Database Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of the database, including the list of tables and their schemas.
+    - To inspect the columns and metadata for specific tables.
+
+Table Used:
+    - INFORMATION_SCHEMA.TABLES
+    - INFORMATION_SCHEMA.COLUMNS
+===============================================================================
 */
 
-USE master;
-GO
+-- Retrieve a list of all tables in the database
+SELECT 
+    TABLE_CATALOG, 
+    TABLE_SCHEMA, 
+    TABLE_NAME, 
+    TABLE_TYPE
+FROM INFORMATION_SCHEMA.TABLES;
 
--- Drop and recreate the 'DataWarehouseAnalytics' database
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouseAnalytics')
-BEGIN
-    ALTER DATABASE DataWarehouseAnalytics SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-    DROP DATABASE DataWarehouseAnalytics;
-END;
-GO
-
--- Create the 'DataWarehouseAnalytics' database
-CREATE DATABASE DataWarehouseAnalytics;
-GO
-
-USE DataWarehouseAnalytics;
-GO
-
--- Create Schemas
-
-CREATE SCHEMA gold;
-GO
-
-CREATE TABLE gold.dim_customers(
-	customer_key int,
-	customer_id int,
-	customer_number nvarchar(50),
-	first_name nvarchar(50),
-	last_name nvarchar(50),
-	country nvarchar(50),
-	marital_status nvarchar(50),
-	gender nvarchar(50),
-	birthdate date,
-	create_date date
-);
-GO
-
-CREATE TABLE gold.dim_products(
-	product_key int ,
-	product_id int ,
-	product_number nvarchar(50) ,
-	product_name nvarchar(50) ,
-	category_id nvarchar(50) ,
-	category nvarchar(50) ,
-	subcategory nvarchar(50) ,
-	maintenance nvarchar(50) ,
-	cost int,
-	product_line nvarchar(50),
-	start_date date 
-);
-GO
-
-CREATE TABLE gold.fact_sales(
-	order_number nvarchar(50),
-	product_key int,
-	customer_key int,
-	order_date date,
-	shipping_date date,
-	due_date date,
-	sales_amount int,
-	quantity tinyint,
-	price int 
-);
-GO
-
-TRUNCATE TABLE gold.dim_customers;
-GO
-
-BULK INSERT gold.dim_customers
-FROM 'C:\sql\sql-data-analytics-project\datasets\csv-files\gold.dim_customers.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-GO
-
-TRUNCATE TABLE gold.dim_products;
-GO
-
-BULK INSERT gold.dim_products
-FROM 'C:\sql\sql-data-analytics-project\datasets\csv-files\gold.dim_products.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-GO
-
-TRUNCATE TABLE gold.fact_sales;
-GO
-
-BULK INSERT gold.fact_sales
-FROM 'C:\sql\sql-data-analytics-project\datasets\csv-files\gold.fact_sales.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-GO
+-- Retrieve all columns for a specific table (dim_customers)
+SELECT 
+    COLUMN_NAME, 
+    DATA_TYPE, 
+    IS_NULLABLE, 
+    CHARACTER_MAXIMUM_LENGTH
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'dim_customers';
